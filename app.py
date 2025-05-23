4daf6dab8a736cdc32b576b1ff9078e7693d0cf8
Jady Nicole Costales <jvcostales@up.edu.ph>
2025-05-24 00:14:32 +0800

    Update app

--- app.py
+++ app.py
@@ -2015,9 +2015,18 @@ def saved():
 
     # 1. Fetch saved posts
     cur.execute("""
-        SELECT images.image_id, images.image_url, images.caption, images.created_at,
-               author.first_name, author.last_name,
-               collaborator.first_name, collaborator.last_name
+        SELECT 
+            images.image_id,              -- image[0]
+            images.image_url,             -- image[1] (not used in this snippet, but useful to have)
+            images.caption,               -- image[2] (not used here)
+            images.created_at,            -- image[3] (not used here)
+            author.id AS author_id,       -- image[4]
+            author.first_name,            -- image[5]
+            author.last_name,             -- image[6]
+            images.created_at,            -- image[7] (used for date formatting)
+            collaborator.id AS collaborator_id,  -- image[8] (check if exists)
+            collaborator.first_name,      -- image[9]
+            collaborator.last_name        -- image[10]
         FROM saved_posts
         JOIN images ON saved_posts.image_id = images.image_id
         JOIN users AS author ON images.id = author.id
