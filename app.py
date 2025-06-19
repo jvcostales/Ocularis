ed70f029ef622733f8e3ea7e535c3ac70c826f39
Jady Nicole Costales <jvcostales@up.edu.ph>
2025-06-19 14:18:14 +0800

    update app

--- app.py
+++ app.py
@@ -2599,7 +2599,7 @@ def saved():
     # 5. Notifications
     cur.execute("""
         SELECT users.first_name || ' ' || users.last_name AS display_name,
-               notifications.action_type, notifications.image_id, notifications.created_at, notifications.actor_id
+               notifications.action_type, notifications.image_id, notifications.created_at, notifications.actor_id, users.profile_pic
         FROM notifications
         JOIN users ON notifications.actor_id = users.id
         WHERE notifications.recipient_id = %s
