a3f86c29bdc623d99c3647418f373274ee9b8367
Jady Nicole Costales <jvcostales@up.edu.ph>
2025-06-23 16:40:25 +0800

    Update app

--- app.py
+++ app.py
@@ -284,7 +284,7 @@ def signup():
         password = generate_password_hash(raw_password)
         token = secrets.token_urlsafe(32)
         default_profile_pic = "pfp.jpg"
-        default_cover_photo = "default_cover.jpg"
+        default_cover_photo = "default_cover.png"
 
         try:
             conn = psycopg2.connect(
