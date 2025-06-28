fa407c2d4ab15943b2eb077345bbde4088896068
AL-Taco <ineed.antonioluis.taco@gmail.com>
2025-06-29 00:53:59 +0800

    Update app

--- app.py
+++ app.py
@@ -817,7 +817,7 @@ def feed_more():
     cur.close()
     conn.close()
 
-    return render_template('partials/_image_posts.html', images=images, saved_image_ids=[], comments=[], comment_likes_data={}, profile_pic_url=url_for('static', filename='pfp.jpg'))
+    return render_template('partials/_image_posts.html', images=images, saved_image_ids=[], comments=[], comment_likes_data={}, profile_pic_url=url_for('static', filename='pfp.jpg'), loop_offset=offset)
 
 @app.route('/post/<int:image_id>')
 def view_post(image_id):
